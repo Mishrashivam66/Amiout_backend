85f0a9bbdec29d74f7faf1124c4b5e91d2f4136c
Shivam Kumar <shivammishra17499@gmail.com>
2026-08-14 15:53:56 +0530

    Allow all origins

--- src/app.js
+++ src/app.js
@@ -34,29 +34,13 @@ const app = express();
 
 app.use(cookieParser());
 
-// Allow multiple frontend URLs
-const allowedOrigins = process.env.FRONTEND_URL
-  ? process.env.FRONTEND_URL.split(",")
-  : [];
-
 app.use(
   cors({
-    origin: (origin, callback) => {
-      // Allow Postman and server-to-server requests
-      if (!origin) {
-        return callback(null, true);
-      }
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("Not allowed by CORS"));
-    },
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  })
+  }),
 );
 
 app.use(helmet());

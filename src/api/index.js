a35a96c36c5c9c794852084a4a5baf3cbbd6b4e7
Shivam Kumar <shivammishra17499@gmail.com>
2026-08-14 19:09:14 +0530

    Test Vercel function

--- src/api/index.js
+++ src/api/index.js
@@ -1,30 +1,6 @@
-require("../config/env");
-
-const connectDatabase = require("../config/db");
-const app = require("../app");
-
-let isConnected = false;
-
-module.exports = async (req, res) => {
-  try {
-    console.log("Function invoked");
-    console.log("NODE_ENV:", process.env.NODE_ENV);
-    console.log("MONGODB_URI exists:", !!process.env.MONGODB_URI);
-    console.log("DB_NAME:", process.env.DB_NAME);
-
-    if (!isConnected) {
-      await connectDatabase();
-      isConnected = true;
-    }
-
-    return app(req, res);
-  } catch (error) {
-    console.error("Vercel Function Error:", error);
-
-    return res.status(500).json({
-      success: false,
-      message: error.message,
-      stack: error.stack,
-    });
-  }
+module.exports = (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Vercel is working",
+  });
 };
